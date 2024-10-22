4a046eac9bf2f31af579f0d6479b751de88a8175
SaeedAlian <aliansaeed82@gmail.com>
2024-10-22 17:56:24 +0330

    chore: add tailwindcss and its dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,76 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const colors = {
+  background: "hsl(var(--background))",
+  foreground: "hsl(var(--foreground))",
+  primary: {
+    light: "hsl(var(--primary-light))",
+    dark: "hsl(var(--primary-dark))",
+    DEFAULT: "hsl(var(--primary))",
+    foreground: "hsl(var(--primary-foreground))",
+    "foreground-light": "hsl(var(--primary-foreground-light))",
+    "foreground-dark": "hsl(var(--primary-foreground-dark))",
+  },
+  secondary: {
+    light: "hsl(var(--secondary-light))",
+    dark: "hsl(var(--secondary-dark))",
+    DEFAULT: "hsl(var(--secondary))",
+    foreground: "hsl(var(--secondary-foreground))",
+    "foreground-light": "hsl(var(--secondary-foreground-light))",
+    "foreground-dark": "hsl(var(--secondary-foreground-dark))",
+  },
+  destructive: {
+    DEFAULT: "hsl(var(--destructive))",
+    foreground: "hsl(var(--destructive-foreground))",
+  },
+  muted: {
+    DEFAULT: "hsl(var(--muted))",
+    foreground: "hsl(var(--muted-foreground))",
+  },
+  accent: {
+    light: "hsl(var(--accent-light))",
+    dark: "hsl(var(--accent-dark))",
+    DEFAULT: "hsl(var(--accent))",
+    foreground: "hsl(var(--accent-foreground))",
+    "foreground-light": "hsl(var(--accent-foreground-light))",
+    "foreground-dark": "hsl(var(--accent-foreground-dark))",
+  },
+  popover: {
+    DEFAULT: "hsl(var(--card))",
+    foreground: "hsl(var(--card-foreground))",
+  },
+  card: {
+    DEFAULT: "hsl(var(--card))",
+    foreground: "hsl(var(--card-foreground))",
+  },
+};
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: "true",
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      borderColor: {
+        ...colors,
+      },
+      colors: {
+        ...colors,
+      },
+      fontFamily: {
+        poppins: ["Poppins", ...fontFamily.sans],
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
