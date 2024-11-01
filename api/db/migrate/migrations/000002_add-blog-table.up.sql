931c72e15bd895f26736bef8a622dee7baf5cf02
SaeedAlian <aliansaeed82@gmail.com>
2024-11-01 20:36:15 +0330

    feat: add blog table migrations

---
+++ api/db/migrate/migrations/000002_add-blog-table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS blogs (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(2047) NOT NULL,
+  slug VARCHAR(255) NOT NULL UNIQUE,
+  pictureName VARCHAR(255) NOT NULL,
+  mdFilename VARCHAR(255) NOT NULL,
+  createdAt TIMESTAMP DEFAULT NOW(),
+  updatedAt TIMESTAMP DEFAULT NOW()
+);
